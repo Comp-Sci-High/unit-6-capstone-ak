4caecfe030df7282dd84ece8fe1e7c966ca6d1ff
akdoingse12 <akdoingse12@gmail.com>
2025-05-14 13:53:44 +0000

    fkemefkme

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+const CommentSchema = new mongoose.Schema({
+  Comment: { type: String },
+  pfpURL: { type: String },
+  rating: { type: Number },
+  App: { type: String },
+});
+
+const Comment = mongoose.model("Comment", CommentSchema, "Comments");
+
