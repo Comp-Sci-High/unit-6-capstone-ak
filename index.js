ecfa72e30c83163a7d6b02d466c3814c96c8f6f9
akdoingse12 <akdoingse12@gmail.com>
2025-05-19 20:47:09 +0000

    Dejhff

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const CommentSchema = new mongoose.Schema({
 
 const Comment = mongoose.model("Comment", CommentSchema, "Comments");
 
-app.comment("/add/comment", async(req,res)=>{
+app.post("/comment/add", async(req,res)=>{
   const comment = await new Comment({
     comment:req.body.comment,
     pfpURL:req.body.pfpURL,
@@ -32,8 +32,42 @@ app.comment("/add/comment", async(req,res)=>{
 res.json(comment)
 })
 
-app.get("/",async(req,res)=>{
+app.get("/comment",async(req,res)=>{
+  const app = await Comment.find({})
+  res.render("comment.ejs",{app})
+})
+app.get("/comments",async(req,res)=>{
   const comments = await Comment.find({})
-  res.render("comments.ejs",{comments})
+  res.json(comments)
 })
 
+app.patch("/menu/update/:comment", async (req, res) => {
+  const updatedComment = await Menu.findOneAndUpdate(
+    { comment: req.params.comment },
+    { pfpURL: res.body.pfpURL },
+    { rating: req.body. rating},
+    { new: true }
+  );
+  res.json(updatedComment);
+});
+
+app.delete("/menu/delete/:comment", async (req, res) => {
+  const deletedComment = await Menu.findOne({
+    comment: req.params.comment,
+  });
+  res.json(deletedComment);
+});
+
+
+
+async function startServer() {
+  await mongoose.connect(
+    "mongodb+srv://SE12:CSH2025@cluster0.u9yhg.mongodb.net/CSHteachers?retryWrites=true&w=majority&appName=Cluster0"
+  );
+
+  app.listen(3000, () => {
+    console.log(`Server running.`);
+  });
+}
+
+startServer();
