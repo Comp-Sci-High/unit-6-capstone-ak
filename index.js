1343226590447d86c7f74305db6c59b541aeb0f0
akdoingse12 <akdoingse12@gmail.com>
2025-05-19 21:06:02 +0000

    efuhefhd

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
+
 const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
